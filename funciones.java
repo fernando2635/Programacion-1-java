77771ba0bb4811e02715792048393bda7fe46a59
Fernando <116363761+fernando2635@users.noreply.github.com>
2026-03-11 16:43:10 -0100

    Add files via upload

---
+++ funciones.java
@@ -0,0 +1,15 @@
+
+public class funciones {
+	//los metodos estan sujetos a la clase
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Funciones fun = new Funciones();
+		//fun.saludar();
+		saludar();
+	}
+	//el public static solo es para definir quien puede llamar a la funcion
+	static void saludar() {
+		System.out.print("hola");
+	}
+	//se puede tener mas de una funcion con el mismo nombre pero se tienen que diferenciar en los parametros de entrada
+}
