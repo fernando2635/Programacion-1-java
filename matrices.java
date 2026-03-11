c546dba4011ae413b086ad74cc090c86f5b91693
Fernando <116363761+fernando2635@users.noreply.github.com>
2026-03-11 17:10:58 -0100

    Add files via upload

---
+++ matrices.java
@@ -0,0 +1,31 @@
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class matrices {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    Scanner sc = new Scanner(System.in);
+	    int[][] matriz = new int[2][3];
+
+	    // Cargar matriz
+	    for (int i = 0; i < 2; i++) {
+	      for (int j = 0; j < 3; j++) {
+	        System.out.print("Ingrese valor [" + i + "][" + j + "]: ");
+	        matriz[i][j] = sc.nextInt();
+	      }
+	    }
+
+	    // Mostrar matriz
+	    System.out.println("Matriz cargada:");
+	    for (int i = 0; i < 2; i++) {
+	      for (int j = 0; j < 3; j++) {
+	        System.out.print(matriz[i][j] + " ");
+	      }
+	      System.out.println();
+	    }
+
+	    sc.close();
+	}
+
+}
